2c2b62f8fe5b6bbe25f87380d386372d92b72469
Igor-Principal <igorprincipal@gmail.com>
2024-02-02 21:17:31 +0200

    created base settings and axios functions

---
+++ src/services/contactsApi.js
@@ -0,0 +1,15 @@
+const { default: axios } = require('axios');
+
+const BASE_URL = 'https://65bbc27e52189914b5bd0541.mockapi.io/api';
+
+export async function getContacts() {
+  return await axios.get(`${BASE_URL}/contacts`);
+}
+
+export async function createContact(data) {
+  return await axios.post(`${BASE_URL}/contacts`, data);
+}
+
+export async function deleteContact(id) {
+  return await axios.delete(`${BASE_URL}/contacts/${id}`);
+}
